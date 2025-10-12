634d8c773a491f6e0180c28b09189c644ad901f1
Willem Beltman <hwj.beltman@gmail.com>
2025-10-12 15:30:40 +0200

    Nice

--- CaptureOnlyMovements/Forms/SettingsForm.Designer.cs
+++ CaptureOnlyMovements/Forms/SettingsForm.Designer.cs
@@ -81,7 +81,7 @@ namespace CaptureOnlyMovements.Forms
             PrefixPresetList.Location = new Point(316, 13);
             PrefixPresetList.Name = "PrefixPresetList";
             PrefixPresetList.RowHeadersWidth = 62;
-            PrefixPresetList.Size = new Size(969, 349);
+            PrefixPresetList.Size = new Size(1024, 212);
             PrefixPresetList.TabIndex = 1;
             // 
             // MinPlaybackSpeedLabel
@@ -89,7 +89,7 @@ namespace CaptureOnlyMovements.Forms
             MinPlaybackSpeedLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             MinPlaybackSpeedLabel.Font = new Font("Segoe UI", 9F);
             MinPlaybackSpeedLabel.ForeColor = Color.WhiteSmoke;
-            MinPlaybackSpeedLabel.Location = new Point(13, 667);
+            MinPlaybackSpeedLabel.Location = new Point(13, 530);
             MinPlaybackSpeedLabel.Name = "MinPlaybackSpeedLabel";
             MinPlaybackSpeedLabel.Size = new Size(287, 23);
             MinPlaybackSpeedLabel.TabIndex = 6;
@@ -102,9 +102,9 @@ namespace CaptureOnlyMovements.Forms
             MinPlaybackSpeed.BorderStyle = BorderStyle.FixedSingle;
             MinPlaybackSpeed.Font = new Font("Segoe UI", 9F);
             MinPlaybackSpeed.ForeColor = Color.WhiteSmoke;
-            MinPlaybackSpeed.Location = new Point(316, 662);
+            MinPlaybackSpeed.Location = new Point(316, 525);
             MinPlaybackSpeed.Name = "MinPlaybackSpeed";
-            MinPlaybackSpeed.Size = new Size(965, 31);
+            MinPlaybackSpeed.Size = new Size(1020, 31);
             MinPlaybackSpeed.TabIndex = 7;
             // 
             // MaximumPixelDifferenceValueLabel
@@ -112,7 +112,7 @@ namespace CaptureOnlyMovements.Forms
             MaximumPixelDifferenceValueLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             MaximumPixelDifferenceValueLabel.Font = new Font("Segoe UI", 9F);
             MaximumPixelDifferenceValueLabel.ForeColor = Color.WhiteSmoke;
-            MaximumPixelDifferenceValueLabel.Location = new Point(13, 377);
+            MaximumPixelDifferenceValueLabel.Location = new Point(13, 240);
             MaximumPixelDifferenceValueLabel.Name = "MaximumPixelDifferenceValueLabel";
             MaximumPixelDifferenceValueLabel.Size = new Size(287, 23);
             MaximumPixelDifferenceValueLabel.TabIndex = 2;
@@ -125,9 +125,9 @@ namespace CaptureOnlyMovements.Forms
             MaximumPixelDifferenceValue.BorderStyle = BorderStyle.FixedSingle;
             MaximumPixelDifferenceValue.Font = new Font("Segoe UI", 9F);
             MaximumPixelDifferenceValue.ForeColor = Color.WhiteSmoke;
-            MaximumPixelDifferenceValue.Location = new Point(316, 372);
+            MaximumPixelDifferenceValue.Location = new Point(316, 235);
             MaximumPixelDifferenceValue.Name = "MaximumPixelDifferenceValue";
-            MaximumPixelDifferenceValue.Size = new Size(965, 31);
+            MaximumPixelDifferenceValue.Size = new Size(1020, 31);
             MaximumPixelDifferenceValue.TabIndex = 3;
             // 
             // MaximumDifferentPixelCountLabel
@@ -135,7 +135,7 @@ namespace CaptureOnlyMovements.Forms
             MaximumDifferentPixelCountLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             MaximumDifferentPixelCountLabel.Font = new Font("Segoe UI", 9F);
             MaximumDifferentPixelCountLabel.ForeColor = Color.WhiteSmoke;
-            MaximumDifferentPixelCountLabel.Location = new Point(13, 426);
+            MaximumDifferentPixelCountLabel.Location = new Point(13, 289);
             MaximumDifferentPixelCountLabel.Name = "MaximumDifferentPixelCountLabel";
             MaximumDifferentPixelCountLabel.Size = new Size(287, 23);
             MaximumDifferentPixelCountLabel.TabIndex = 4;
@@ -148,9 +148,9 @@ namespace CaptureOnlyMovements.Forms
             MaximumDifferentPixelCount.BorderStyle = BorderStyle.FixedSingle;
             MaximumDifferentPixelCount.Font = new Font("Segoe UI", 9F);
             MaximumDifferentPixelCount.ForeColor = Color.WhiteSmoke;
-            MaximumDifferentPixelCount.Location = new Point(316, 421);
+            MaximumDifferentPixelCount.Location = new Point(316, 284);
             MaximumDifferentPixelCount.Name = "MaximumDifferentPixelCount";
-            MaximumDifferentPixelCount.Size = new Size(965, 31);
+            MaximumDifferentPixelCount.Size = new Size(1020, 31);
             MaximumDifferentPixelCount.TabIndex = 5;
             // 
             // EncoderComboLabel
@@ -158,7 +158,7 @@ namespace CaptureOnlyMovements.Forms
             EncoderComboLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             EncoderComboLabel.Font = new Font("Segoe UI", 9F);
             EncoderComboLabel.ForeColor = Color.WhiteSmoke;
-            EncoderComboLabel.Location = new Point(13, 524);
+            EncoderComboLabel.Location = new Point(13, 387);
             EncoderComboLabel.Name = "EncoderComboLabel";
             EncoderComboLabel.Size = new Size(287, 23);
             EncoderComboLabel.TabIndex = 8;
@@ -172,9 +172,9 @@ namespace CaptureOnlyMovements.Forms
             EncoderCombo.FlatStyle = FlatStyle.Flat;
             EncoderCombo.ForeColor = Color.WhiteSmoke;
             EncoderCombo.Items.AddRange(new object[] { "software", "nvidia", "amd", "intel" });
-            EncoderCombo.Location = new Point(316, 519);
+            EncoderCombo.Location = new Point(316, 382);
             EncoderCombo.Name = "EncoderCombo";
-            EncoderCombo.Size = new Size(965, 33);
+            EncoderCombo.Size = new Size(1020, 33);
             EncoderCombo.TabIndex = 9;
             // 
             // PresetComboLabel
@@ -182,7 +182,7 @@ namespace CaptureOnlyMovements.Forms
             PresetComboLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             PresetComboLabel.Font = new Font("Segoe UI", 9F);
             PresetComboLabel.ForeColor = Color.WhiteSmoke;
-            PresetComboLabel.Location = new Point(13, 619);
+            PresetComboLabel.Location = new Point(13, 482);
             PresetComboLabel.Name = "PresetComboLabel";
             PresetComboLabel.Size = new Size(287, 23);
             PresetComboLabel.TabIndex = 10;
@@ -196,9 +196,9 @@ namespace CaptureOnlyMovements.Forms
             PresetCombo.FlatStyle = FlatStyle.Flat;
             PresetCombo.ForeColor = Color.WhiteSmoke;
             PresetCombo.Items.AddRange(new object[] { "veryfast", "faster", "fast", "medium", "slow", "slower", "veryslow" });
-            PresetCombo.Location = new Point(316, 614);
+            PresetCombo.Location = new Point(316, 477);
             PresetCombo.Name = "PresetCombo";
-            PresetCombo.Size = new Size(965, 33);
+            PresetCombo.Size = new Size(1020, 33);
             PresetCombo.TabIndex = 11;
             // 
             // QualityComboLabel
@@ -206,7 +206,7 @@ namespace CaptureOnlyMovements.Forms
             QualityComboLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             QualityComboLabel.Font = new Font("Segoe UI", 9F);
             QualityComboLabel.ForeColor = Color.WhiteSmoke;
-            QualityComboLabel.Location = new Point(13, 571);
+            QualityComboLabel.Location = new Point(13, 434);
             QualityComboLabel.Name = "QualityComboLabel";
             QualityComboLabel.Size = new Size(287, 23);
             QualityComboLabel.TabIndex = 12;
@@ -220,9 +220,9 @@ namespace CaptureOnlyMovements.Forms
             QualityCombo.FlatStyle = FlatStyle.Flat;
             QualityCombo.ForeColor = Color.WhiteSmoke;
             QualityCombo.Items.AddRange(new object[] { "identical", "high", "medium", "low", "lower", "verylow" });
-            QualityCombo.Location = new Point(316, 566);
+            QualityCombo.Location = new Point(316, 429);
             QualityCombo.Name = "QualityCombo";
-            QualityCombo.Size = new Size(965, 33);
+            QualityCombo.Size = new Size(1020, 33);
             QualityCombo.TabIndex = 13;
             // 
             // FpsComboLabel
@@ -230,7 +230,7 @@ namespace CaptureOnlyMovements.Forms
             FpsComboLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             FpsComboLabel.Font = new Font("Segoe UI", 9F);
             FpsComboLabel.ForeColor = Color.WhiteSmoke;
-            FpsComboLabel.Location = new Point(13, 474);
+            FpsComboLabel.Location = new Point(13, 337);
             FpsComboLabel.Name = "FpsComboLabel";
             FpsComboLabel.Size = new Size(287, 23);
             FpsComboLabel.TabIndex = 14;
@@ -244,9 +244,9 @@ namespace CaptureOnlyMovements.Forms
             FpsCombo.FlatStyle = FlatStyle.Flat;
             FpsCombo.ForeColor = Color.WhiteSmoke;
             FpsCombo.Items.AddRange(new object[] { "25", "30", "50", "60" });
-            FpsCombo.Location = new Point(316, 469);
+            FpsCombo.Location = new Point(316, 332);
             FpsCombo.Name = "FpsCombo";
-            FpsCombo.Size = new Size(965, 33);
+            FpsCombo.Size = new Size(1020, 33);
             FpsCombo.TabIndex = 15;
             // 
             // Note1
@@ -254,9 +254,9 @@ namespace CaptureOnlyMovements.Forms
             Note1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             Note1.Font = new Font("Segoe UI", 9F);
             Note1.ForeColor = Color.LightGray;
-            Note1.Location = new Point(330, 712);
+            Note1.Location = new Point(330, 575);
             Note1.Name = "Note1";
-            Note1.Size = new Size(951, 23);
+            Note1.Size = new Size(1006, 23);
             Note1.TabIndex = 16;
             Note1.Text = "Note: Realtime (value 1) will be difficult because the ";
             // 
@@ -265,9 +265,9 @@ namespace CaptureOnlyMovements.Forms
             Note2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             Note2.Font = new Font("Segoe UI", 9F);
             Note2.ForeColor = Color.LightGray;
-            Note2.Location = new Point(330, 737);
+            Note2.Location = new Point(330, 600);
             Note2.Name = "Note2";
-            Note2.Size = new Size(951, 23);
+            Note2.Size = new Size(1006, 23);
             Note2.TabIndex = 17;
             Note2.Text = "frames are being compared single threaded with no bufferpipeline.";
             // 
@@ -276,9 +276,9 @@ namespace CaptureOnlyMovements.Forms
             Note3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             Note3.Font = new Font("Segoe UI", 9F);
             Note3.ForeColor = Color.LightGray;
-            Note3.Location = new Point(330, 762);
+            Note3.Location = new Point(330, 625);
             Note3.Name = "Note3";
-            Note3.Size = new Size(951, 23);
+            Note3.Size = new Size(1006, 23);
             Note3.TabIndex = 18;
             Note3.Text = "Please use OBS for realtime capturing.";
             // 
@@ -290,9 +290,9 @@ namespace CaptureOnlyMovements.Forms
             SaveButton.FlatStyle = FlatStyle.Flat;
             SaveButton.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
             SaveButton.ForeColor = Color.WhiteSmoke;
-            SaveButton.Location = new Point(316, 797);
+            SaveButton.Location = new Point(316, 660);
             SaveButton.Name = "SaveButton";
-            SaveButton.Size = new Size(967, 65);
+            SaveButton.Size = new Size(1022, 65);
             SaveButton.TabIndex = 19;
             SaveButton.Text = "Apply and close";
             SaveButton.UseVisualStyleBackColor = false;
@@ -302,7 +302,7 @@ namespace CaptureOnlyMovements.Forms
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(24, 24, 24);
-            ClientSize = new Size(1298, 884);
+            ClientSize = new Size(1353, 747);
             Controls.Add(PrefixPresetListLabel);
             Controls.Add(PrefixPresetList);
             Controls.Add(MaximumPixelDifferenceValueLabel);
@@ -326,7 +326,6 @@ namespace CaptureOnlyMovements.Forms
             Font = new Font("Segoe UI", 9F);
             ForeColor = Color.WhiteSmoke;
             FormBorderStyle = FormBorderStyle.SizableToolWindow;
-            //MinimumSize = new Size(1320, 940);
             Name = "ConfigForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Settings";
